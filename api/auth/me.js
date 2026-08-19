294b175b1136ea7fbe06be059958a930c37e46a2
kabakaro12 <61512272+kabakaro12@users.noreply.github.com>
2026-08-19 23:09:12 +0200

    Delete api/auth/me.js

--- api/auth/me.js
+++
@@ -1,8 +0,0 @@
-const { requireUser, sendError } = require("../../lib/auth");
-module.exports = async function handler(req,res){
-  if(req.method!=="GET") return res.status(405).json({ok:false,error:"Méthode non autorisée"});
-  try {
-    const u=requireUser(req);
-    res.json({ok:true,user:{id:u.sub,email:u.email,firstName:u.firstName,lastName:u.lastName}});
-  } catch(err){ sendError(res,err); }
-};
